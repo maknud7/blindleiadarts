da94b9d229e895e1d657fd553937097fcb8ec0ca
maknud7 <magnus@ingenting.org>
2026-08-23 22:05:54 +0200

    Add DartsAtlas connector config

---
+++ packages/connectors/src/DartsAtlas/DartsAtlasConfig.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blindleia\Dartkiosk\Connectors\DartsAtlas;
+
+final class DartsAtlasConfig
+{
+    public function __construct(
+        private readonly string $seasonId = '',
+        private readonly string $tournamentId = '',
+        private readonly int $clubId = 0,
+        private readonly ?int $localSeasonId = null,
+        private readonly string $membersTable = 'medlemmer',
+        private readonly int $pollIntervalSeconds = 8,
+        private readonly string $userAgent = 'BlindleiaDarts/1.0',
+    ) {}
+
+    public function seasonId(): string
+    {
+        return $this->seasonId;
+    }
+
+    public function tournamentId(): string
+    {
+        return $this->tournamentId;
+    }
+
+    public function clubId(): int
+    {
+        return $this->clubId;
+    }
+
+    public function localSeasonId(): ?int
+    {
+        return $this->localSeasonId;
+    }
+
+    public function membersTable(): string
+    {
+        return $this->membersTable;
+    }
+
+    public function pollIntervalSeconds(): int
+    {
+        return max(5, $this->pollIntervalSeconds);
+    }
+
+    public function userAgent(): string
+    {
+        return $this->userAgent;
+    }
+}
