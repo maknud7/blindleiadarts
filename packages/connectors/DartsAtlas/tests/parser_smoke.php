66e3a9f615e723ef5e8ed7ba94d232ffb32958a0
maknud7 <magnus@ingenting.org>
2026-08-23 21:50:01 +0200

    Test parsing av live kamprad

--- packages/connectors/DartsAtlas/tests/parser_smoke.php
+++ packages/connectors/DartsAtlas/tests/parser_smoke.php
@@ -18,9 +18,34 @@ assert($season['players'][0]['external_id'] === 'player-1');
 assert($season['players'][0]['name'] === 'Magnus Knudsen');
 assert($season['tournaments'][0]['external_id'] === 'tournament-1');
 
+$tournamentHtml = <<<'HTML'
+<html><head><title>Mandag 24. august</title></head><body>
+<a href="/tournaments/tournament-1/player_stats/player-1">Magnus Knudsen</a>
+<a href="/tournaments/tournament-1/player_stats/player-2">Thomas Kildal</a>
+<div class="match-row">
+  <span>Board 2 Round 1 Best of 3</span>
+  <a href="/tournaments/tournament-1/player_stats/player-1">Magnus Knudsen</a> 1
+  <a href="/tournaments/tournament-1/player_stats/player-2">Thomas Kildal</a> 1
+  <span>58.42 Avg 55.10 Avg</span>
+  <a href="/matches/match-1">Match</a>
+</div>
+</body></html>
+HTML;
+$tournament = $parser->parseTournament($tournamentHtml, 'https://www.dartsatlas.com/tournaments/tournament-1');
+assert(count($tournament['players']) === 2);
+assert(count($tournament['matches']) === 1);
+assert($tournament['matches'][0]['external_id'] === 'match-1');
+assert($tournament['matches'][0]['board_number'] === 2);
+assert($tournament['matches'][0]['round_label'] === 'Round 1');
+assert($tournament['matches'][0]['status'] === 'in_progress');
+assert($tournament['matches'][0]['player_a_legs'] === 1);
+assert($tournament['matches'][0]['player_b_legs'] === 1);
+assert($tournament['matches'][0]['average_a'] === 58.42);
+assert($tournament['matches'][0]['average_b'] === 55.10);
+
 $broadcastHtml = <<<'HTML'
 <div data-player-id="player-1" data-player-name="Magnus Knudsen" data-score="141" data-legs="2" data-average="58.42" data-score-180="1"></div>
-<div data-player-id="player-2" data-player-name="Test Player" data-score="204" data-legs="1" data-average="55,10"></div>
+<div data-player-id="player-2" data-player-name="Thomas Kildal" data-score="204" data-legs="1" data-average="55,10"></div>
 HTML;
 $broadcast = $parser->parseBroadcast($broadcastHtml, 'match-1');
 assert($broadcast['external_id'] === 'match-1');
