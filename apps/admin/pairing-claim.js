14b2c83f7e97261a4c81dece7421eda8b7cd5e89
maknud7 <magnus@ingenting.org>
2026-08-25 10:21:07 +0200

    Load tournament ELO admin controls

--- apps/admin/pairing-claim.js
+++ apps/admin/pairing-claim.js
@@ -2,4 +2,5 @@ import "./pairing-claim-core.js";
 import "./board-admin.js";
 import "./member-onboarding-admin.js";
 import "./tournament-admin.js";
+import "./tournament-elo-admin.js";
 import "./tournament-summary-admin.js";
