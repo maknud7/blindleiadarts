c40a2893dd2bb704359dbdc5a362b62616f33860
maknud7 <magnus@ingenting.org>
2026-09-01 15:13:54 +0200

    Mark legacy board cleanup as TEST-only

--- infra/sql/migrate.php
+++ infra/sql/migrate.php
@@ -104,6 +104,7 @@ $testOnlyNoopMigrations = [
     '0058_reconcile_test_elo_after_historical_import.php',
     '0059_reconcile_test_elo_for_prod_readiness.php',
     '0068_cleanup_test_champion_player_labels.php',
+    '0073_cleanup_test_legacy_board_materialization.php',
 ];
 
 $appliedCount = 0;
