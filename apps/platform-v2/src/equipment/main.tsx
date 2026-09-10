0977102e5a287d743b959922b49eac1209763c36
maknud7 <magnus@ingenting.org>
2026-09-10 09:26:39 +0200

    Load Equipment v2 Scolia styles

--- apps/platform-v2/src/equipment/main.tsx
+++ apps/platform-v2/src/equipment/main.tsx
@@ -2,5 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { EquipmentWorkspace } from "./EquipmentWorkspace";
 import "../shared/styles.css";
+import "./scolia.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><EquipmentWorkspace /></StrictMode>);
