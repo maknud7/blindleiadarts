cfdf22799c5c3ec14826a519859542aed72f0f4a
maknud7 <magnus@ingenting.org>
2026-09-10 16:11:42 +0200

    Restore editable last four visits in Kiosk v2 (#197)
    * Restore editable recent visits in Kiosk v2
    
    * Style Kiosk v2 recent visit correction
    
    * Mount editable recent visits in Kiosk v2

--- apps/platform-v2/src/kiosk/main.tsx
+++ apps/platform-v2/src/kiosk/main.tsx
@@ -2,17 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { KioskErrorBoundary } from "./KioskErrorBoundary";
 import { KioskWorkspace } from "./KioskWorkspace";
+import { RecentVisitEditor } from "./RecentVisitEditor";
 import { initializeTestHandoff } from "./testHandoff";
 import "../shared/styles.css";
 import "./scolia-runtime.css";
 import "./kiosk-tablet.css";
 import "./kiosk-interactions.css";
+import "./recent-visit-editor.css";
 
 if (initializeTestHandoff()) {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <KioskErrorBoundary>
         <KioskWorkspace />
+        <RecentVisitEditor />
       </KioskErrorBoundary>
     </StrictMode>,
   );
