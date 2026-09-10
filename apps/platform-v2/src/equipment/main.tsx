d9aab32293b2440e0014721a64b162a22bb237af
maknud7 <magnus@ingenting.org>
2026-09-10 11:27:08 +0200

    Add TEST navigation to Equipment v2

--- apps/platform-v2/src/equipment/main.tsx
+++ apps/platform-v2/src/equipment/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { EquipmentWorkspace } from "./EquipmentWorkspace";
+import { V2TestNavigation } from "../shared/V2TestNavigation";
 import "../shared/styles.css";
 import "./scolia.css";
 import "./board-editor.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode><EquipmentWorkspace /></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <V2TestNavigation active="equipment" />
+    <EquipmentWorkspace />
+  </StrictMode>
+);
