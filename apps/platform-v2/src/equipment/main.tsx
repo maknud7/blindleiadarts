4c8039af2ddd1a455a466a9e8a6e4f78004c043b
maknud7 <magnus@ingenting.org>
2026-09-10 09:47:18 +0200

    Load board editor styles

--- apps/platform-v2/src/equipment/main.tsx
+++ apps/platform-v2/src/equipment/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import { EquipmentWorkspace } from "./EquipmentWorkspace";
 import "../shared/styles.css";
 import "./scolia.css";
+import "./board-editor.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><EquipmentWorkspace /></StrictMode>);
