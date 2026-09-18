3766b03fb2e35d353f425a69517cf57f3ae3139a
maknud7 <magnus@ingenting.org>
2026-09-18 06:43:55 +0200

    Remove Challonge API configuration

--- apps/api/src/Support/Config.php
+++ apps/api/src/Support/Config.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Blindleia\Dartkiosk\Api\Support;
 
-use Blindleia\Dartkiosk\Connectors\Challonge\ChallongeConfig;
 use RuntimeException;
 
 final class Config
@@ -177,21 +176,5 @@ final class Config
         return (string) (($this->config['backend_v2']['internal_token'] ?? '') ?: '');
     }
 
-    public function challonge(): ChallongeConfig
-    {
-        /** @var array<string, mixed> $challonge */
-        $challonge = is_array($this->config['challonge'] ?? null) ? $this->config['challonge'] : [];
-        /** @var array<int, string> $defaultScopes */
-        $defaultScopes = is_array($challonge['default_scopes'] ?? null) ? $challonge['default_scopes'] : [];
 
-        return new ChallongeConfig(
-            (string) (($challonge['api_base_url'] ?? 'https://api.challonge.com/v2.1') ?: 'https://api.challonge.com/v2.1'),
-            (string) (($challonge['oauth_authorize_url'] ?? 'https://api.challonge.com/oauth/authorize') ?: 'https://api.challonge.com/oauth/authorize'),
-            (string) (($challonge['oauth_token_url'] ?? 'https://api.challonge.com/oauth/token') ?: 'https://api.challonge.com/oauth/token'),
-            (string) (($challonge['redirect_uri'] ?? '') ?: ''),
-            (string) (($challonge['client_id'] ?? '') ?: ''),
-            (string) (($challonge['client_secret'] ?? '') ?: ''),
-            $defaultScopes
-        );
-    }
 }
